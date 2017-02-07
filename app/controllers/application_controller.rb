930541f24f92a71d72798646e91315cb2f82dc74
g-mehra <gmehra@alumni.uwo.ca>
2017-02-06 23:41:30 -0500

    Assignment complete - error when posting reviews needs to be corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,14 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user  
+  helper_method :current_user
+
+  def ensure_logged_in
+   unless current_user
+     flash[:alert] = "Please log in"
+     redirect_to new_session_url
+   end
+  end
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+
   has_secure_password
 
   has_many :reviews
-  has_many :products, through: :reviews 
+  has_many :products, through: :reviews
+
+  validates :name, presence: true
+
 end
